a934550a200d496889f4cb8c5efc4b78bad12f9a
moonlightsonata-751 <76097977+moonlightsonata-751@users.noreply.github.com>
2023-08-02 12:16:19 +0530

    Update EmpRecordManCode.cpp
    Employee Record Management System using C++

--- EmpRecordManCode.cpp
+++ EmpRecordManCode.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+ 
 string id[30], name[30], address[50], dob[30], mobile_no[30], doj[30], marstatus[30], workd[30], workl[40], ctc[30], socialins[30], email[30]; // arrays for storing students data.
 
 int total = 0; // to store all data
